358d41b6d43b96020d1b106a0ce5ba6bc68c1035
mohamadhassan2 <mhassan@abstract.security>
2025-03-24 02:53:55 -0500

    Update my_code.py
    -In progress: Started the work with cli arguments. When finished we should be able to specify: Output_file_name and type (json, csv). And debug option to show execution details. Running with arg has defualts

--- my_code.py
+++ my_code.py
@@ -1,3 +1,4 @@
+from xml.etree.ElementTree import tostring
 import requests
 from requests.auth import HTTPBasicAuth
 import csv
@@ -11,6 +12,7 @@ import logging
 import signal
 import sys
 import shutil
+import argparse
 
 # GitHub Advisory Database API URL
 GITHUB_ADVISORY_URL = "https://api.github.com/advisories"
@@ -20,9 +22,7 @@ GITHUB_ADVISORY_URL = "https://api.github.com/advisories"
 CISA_KEV_URL = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
 
 #------------------------------------------------------------------------------
-# Function to fetch vulnerabilities from GitHub Advisory Database
-#https://www.w3schools.com/python/ref_requests_response.asp
-
+# Function to fetch vulnerabilities from GitHub Advisory Database https://www.w3schools.com/python/ref_requests_response.asp
 def fetch_github_advisories():
     advisories = []
     page = 1
@@ -116,31 +116,42 @@ def categorize_and_zip(advisories, kev_ids):
                     advisory_filename = f"{severity}/{cve_id}.json"
                     #--------------------------------------------------------------------------------------
                     directory_path = f"{severity}"
-                    print (directory_path, advisory_filename)
-                    if os.path.exists(directory_path):
-                        try:
-                            shutil.rmtree(directory_path)
-                            print(f"Directory '{directory_path}' and its contents deleted successfully.")
-                            logging.info(f"Directory '{directory_path}' and its contents deleted successfully.")
-                        except Exception as e:
-                            print(f"Error deleting directory '{directory_path}': {e}")
-                            logging.error (f"Error deleting directory '{directory_path}': {e}")
-                    else:
-                        print(f"Directory '{directory_path}' does not exist.")
-                        logging.warning (f"Directory '{directory_path}' does not exist.")
+                 #   print ("Processing>> Directory path: ", directory_path, "  Advisory filename: ",advisory_filename)
+                    #if os.path.exists(directory_path):
+                    #    try:
+                    #        shutil.rmtree(directory_path)
+                    #        print(f"Directory '{directory_path}' and its contents deleted successfully.")
+                    #        logging.info(f"Directory '{directory_path}' and its contents deleted successfully.")
+                    #    except Exception as e:
+                    #        print(f"Error deleting directory '{directory_path}': {e}")
+                    #        logging.error (f"Error deleting directory '{directory_path}': {e}")
+                    #else:
+                    #    print(f"Directory '{directory_path}' does not exist.")
+                    #    logging.warning (f"Directory '{directory_path}' does not exist.")
                     #--------------------------------------------------------------------------------------
 
                     with open(advisory_filename, 'w') as f:
                         json.dump(advisory, f)  # Store the advisory as JSON
+                        logging.info (f"Dumping {advisory} as json into {advisory_filename}")
+
                     zipf.write(advisory_filename)
+                    logging.info (f"Zipping into {advisory_filename}")
 
     return csv_data
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
-# Function to generate the CSV file
-def generate_csv(csv_data):
+def count_lines(output_file_name):
+
+    #Count the lines in CSV file to see if has been populated
+    with open(output_file_name, 'r') as fp:
+        lines = len (fp.readlines()) - 1
+    return lines
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+# Function to generate the vuln CSV file
+def generate_csv(csv_data, output_file_name):
     fieldnames = ['CVE ID', 'Severity', 'Description', 'Vulnerable Package', 'Published At', 'Updated At', 'KEV']
-    csv_filename = "vulnerabilities.csv"
+    csv_filename = output_file_name
 
     with open(csv_filename, mode='w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
@@ -148,9 +159,8 @@ def generate_csv(csv_data):
         for row in csv_data:
             writer.writerow(row)
 
-    #Count the lines in CSV file to see if has been populated
-    with open(csv_filename, 'r') as fp:
-        lines = len (fp.readlines()) - 1
+    lines = count_lines(csv_filename)
+
     if lines == 0:
         print(f"CSV file '{csv_filename}' has been created successfully! If fetching advisories failed in above step; then file will be empty! [\033[33;4mlines count:{lines}\033[0m] \n")
         logging.warning (f"CSV file '{csv_filename}' has been created successfully! If fetching advisories failed in above step; then file will be empty! [\033[33;4mlines count:{lines}\033[0m]")
@@ -158,6 +168,7 @@ def generate_csv(csv_data):
         print(f"CSV file '{csv_filename}' created successfully! If fetching advisories failed in above step; then file will be empty [lines count:{lines}] \n")
         logging.info (f"CSV file '{csv_filename}' created successfully! If fetching advisories failed in above step; then file will be empty [lines count:{lines}]")
 
+    return lines
 '''
 #------------------test
     try:
@@ -181,6 +192,7 @@ def generate_csv(csv_data):
 '''
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
+#Function to check info for GitHUB rate limitations
 def check_rate_limit(github_token=None):
     '''
     Checks the rate limit status for the GitHub API.
@@ -208,6 +220,7 @@ def check_rate_limit(github_token=None):
         logging.error (f"\033[41;37mError: Failed to retrieve rate limit information. Status code: [\033[33;4m{response.status_code}]\033[0m")
         return None
 #------------------------------------------------------------------------------
+#Function to determine how long to pause between git api call (if you need to).
 def run_timer(timer_in_sec):
     print (f"\033[41;37mPrimary or Secondary GitHub API Rate Limit Reached. Results maybe incomplete!\033[0m")
     logging.info (f"\033[41;37mPrimary or Secondary GitHub API Rate Limit Reached. Results maybe incomplete!\033[0m")
@@ -225,18 +238,35 @@ def signal_handler(sig, frame):
     sys.exit(0)
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
+#Function to authenticate to GitHub.
 def authenticate_git():
-    #------------------------------------------
-    #gh auth login --with-token < git_token.txt
+    token_filename = "github_token.txt"
+    if os.path.exists(token_filename):
+        try:
+            with open(token_filename, 'r') as file:
+                first_line = file.readline()
+                os.environ['GITHUB_TOKEN'] = first_line
+                print("GitHub token file detected. Setting up the env var...")
+                logging.info("GitHub token file detected. Setting up the env var...")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    else:
+        print(f"File '{token_filename}' does not exist.")
+
     try:
         token = os.environ['GITHUB_TOKEN']
     except KeyError:
         while True:
             token = input(f"env GITHUB_TOKEN not set. Please set it in your shell (bash: export GITHUB_TOKEN=\"xxxxx\") or enter token now: ")
             if len (token) == 40 :
+                print (f"Saving token to {token_filename} for further use")
+                logging.INFO (f"Saving token to {token_filename} for further use")
+                with open(token_filename, "w") as file:
+                    file.write(token)
                 break
             else:
                 print (f"\n[{token}][{len(token)}] Must be at least 40 characters! Please re-enter:")
+
 
     headers = {'Authorization': f'token {token}'}
     response = requests.get('https://api.github.com/user', headers=headers)
@@ -251,6 +281,7 @@ def authenticate_git():
     return  token
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
+#Function to setup python logging. Support all common types. This should be use to troubleshooting and debugging.
 def setup_logging():
     #Setup logging
     logger = logging.getLogger(__name__)
@@ -259,22 +290,37 @@ def setup_logging():
     logfile += ".log"
     logging.basicConfig(format='%(asctime)s |%(levelname)s| %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p', filename=logfile, encoding='utf-8', level=logging.DEBUG)
     #logger.debug('This message should go to the log file')
-    logger.info('------------- STARTED NEW CODE EXECUTION  ---------\n')
+    logger.info('-----------------  STARTED  ---------\n')
     #logger.warning('And this, too')
     #logger.error('And non-ASCII stuff, too, like Øresund and Malmö')
     return
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
+#Function to setup signal traps. We need to know when user hit CTRL-C
 def setup_signal_handling():
     signal.signal(signal.SIGINT, signal_handler)
     #print('Press Ctl+C')
     #singal.pause()
     return
 #------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
+# #------------------------------------------------------------------------------
 
 # Main function to orchestrate the script
 def main():
+
+    this_script_name = os.path.basename(__file__)
+    parser = argparse.ArgumentParser(prog=this_script_name, description='For details on this script see: https://github.com/mohamadhassan2/download-github-security-vulnerabilities-/blob/main/README.md ')
+
+    parser.add_argument("-o", "--output_file", type=str, default='vulnerabilites', help="The output file name to save the results. [default: csv]", required=False )
+    parser.add_argument('-t', '--type', type=str, default='csv', help="The output file type to save the results. Can be [json] or [csv:default]", required=False )      #option that takes a value
+    parser.add_argument('-d', '--debug', default='False', action='store_true', help="Show extra debug information. [default: false]", required=False )   #on/off flag
+    args = parser.parse_args()
+    DEBUG = args.debug          #Used as global variable
+    output_file_name = (args.output_file + "." + args.type)
+    print(f"[Output_file: {output_file_name}]   [Type: {args.type}]   [Debug: {args.debug}]")
+    print ("\n")
 
     x = setup_signal_handling()
     x = setup_logging()
@@ -318,10 +364,10 @@ def main():
     if (status_code != 200) and (user_answer == "y"):
         user_answer = run_timer(timer_in_sec)
         print("\033[92m>>2nd Fetching GitHub Advisories Pages (may take time and subject to API rate limits)...\033[33;4m[Previous Status Code{status_code}]\033[00m", end="" )
-        logging.info("\033[92m>>2nd Fetching GitHub Advisories Pages (may take time and subject to API rate limits)...[\033[33;4mPrevious Status Code{status_code}]\033[00m")
+        logging.info("\033[92m>>2nd Fetching GitHub Advisories Pages (may take time and subject to API rate limits)...[\033[33;4mPrevious Status Code {status_code}]\033[00m")
         advisories, status_code = fetch_github_advisories()
-        print("\033[92m>>2nd Fetching GitHub Advisories Pages Result...\033[33;4m[Status Code{status_code}] \033[00m", end="" )
-        logging.info ("\033[92m>>2nd Fetching GitHub Advisories Pages Result...[\033[33;4mStatus Code{status_code}]\033[00m")
+        print("\033[92m>>2nd Fetching GitHub Advisories Pages Result...\033[33;4m[Status Code {status_code}] \033[00m")
+        logging.info ("\033[92m>>2nd Fetching GitHub Advisories Pages Result...[\033[33;5mStatus Code{status_code}]\033[00m")
 
     print(f"\033[93m>>Fetching CISA Known Exploited Vulnerabilities (KEV)...\033[00m", end="" )
     logging.info (f"\033[93m>>Fetching CISA Known Exploited Vulnerabilities (KEV)...\033[00m")
@@ -334,8 +380,13 @@ def main():
 
     print("\033[95m>>Generating CSV file with vulnerability data..\033[00m \n")
     logging.info ("\033[95m>>Generating CSV file with vulnerability data..\033[00m")
-    generate_csv(csv_data)
+    lines = generate_csv(csv_data, output_file_name)
+    if DEBUG:
+        print ("csv_data:" , csv_data)
+
+    lines = count_lines(output_file_name)
+    print (f"------ FINISHED ----- Results: [Out FileName:{output_file_name} Len:{lines}] [CISA kev_ids:{len(kev_ids)}] [GitHub Advisories:{len(advisories)}]  ------\n ")
+    logging.info (f"------ FINISHED ----- Results: [Out FileName:{output_file_name} Len:{lines}] [CISA kev_ids:{len(kev_ids)}] [GitHub Advisories:{len(advisories)}]  ------\n ")
 
 if __name__ == "__main__":
     main()
-    
